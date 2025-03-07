a67931728737fb03843529190824a24c733deea8
exzaell <alexs.m@hotmail.com>
2025-03-07 17:16:20 -0500

    ajustando banco de dados - segunda parte

--- projeto_ufac/settings.py
+++ projeto_ufac/settings.py
@@ -101,23 +101,18 @@ WSGI_APPLICATION = 'projeto_ufac.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
-    # https://github.com/jazzband/dj-database-url
     DATABASES = {
-        "default": dj_database_url.config(
-            env="DATABASE_URL",
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-        "ENGINE": "django.db.backends.postgresql"
+        "default": {
+            **dj_database_url.config(
+                env="DATABASE_URL",
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True,
+            ),
+            "ENGINE": "django.db.backends.postgresql",  # Adicione explicitamente o ENGINE
+        }
     }
 else:
-    # When running locally in development or in CI, a sqlite database file will be used instead
-    # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
